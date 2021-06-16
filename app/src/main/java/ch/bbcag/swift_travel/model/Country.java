63bb245b6574f3751c886db6cc7b623f2ef13dc2
Jan <jan.henke@bbcag.ch>
2021-06-16 09:55:06 +0200

    Update country opbject

--- app/src/main/java/ch/bbcag/swift_travel/model/Country.java
+++ app/src/main/java/ch/bbcag/swift_travel/model/Country.java
@@ -1,5 +1,54 @@
 package ch.bbcag.swift_travel.model;
 
+import android.net.Uri;
+
+import java.util.List;
+
 public class Country {
     private String name;
+    private String duration;
+    private Uri imageURI;
+    private List<City> cityList;
+
+    public Country() {
+
+    }
+
+    public Country(String name, String duration, Uri imageURI) {
+        this.name = name;
+        this.duration = duration;
+        this.imageURI = imageURI;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Uri getImageURI() {
+        return imageURI;
+    }
+
+    public void setImageURI(Uri imageURI) {
+        this.imageURI = imageURI;
+    }
+
+    public List<City> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(List<City> cityList) {
+        this.cityList = cityList;
+    }
 }
